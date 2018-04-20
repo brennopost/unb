3437c5b1de62508e46bb43796de73693d09a26fa
Brenno Cordeiro <brennopost@gmail.com>
2018-04-20 15:05:31 -0300

    lista 3 atualizada

--- icc/lista3.py
+++ icc/lista3.py
@@ -15,11 +15,15 @@ print("Parabéns! Você acertou.")
 
 n = []
 i = int(input())
-while i != 0:
-    n.append(i)
-    i = int(input())
+if i != 0:
+    while i != 0:
+        n.append(i)
+        i = int(input())
+    print("{}\n{}\n{:.2f}".format(len(n), max(n), sum(n)/len(n)))
 
-print("{}\n{}\n{:.2f}".format(len(n), max(n), sum(n)/len(n)))
+else:
+    print("0\n0\n0.00")
+
 
 # %% QUESTAO C
 
@@ -78,7 +82,32 @@ if n > 2:
         s = a + b
         b = a
         a = s
-print("{} {} {k} ".format(s, factorial(n), k = b if s % 2 == 0 else ""))
+    print("{} {}{k}".format(s, factorial(n), k = " {}".format(b) if s % 2 == 0 else ""))
+elif n == 1:
+    print("1 1")
+else:
+    print("1 1 1")
+
+# %% QUESTAO E REFAZIDA
+from math import factorial
+
+n = int(input())
+
+a = 1
+b = 1
+s = 0
+if n > 2:
+    for i in range(n - 2):
+        s = a + b
+        b = a
+        a = s
+    print("{} {}".format(s, factorial(n)), end="")
+    if s % 2 == 0:
+        print(" {}".format(b))
+elif n == 1:
+    print("1 1")
+else:
+    print("1 2")
 
 # %% QUESTAO F
 
@@ -107,3 +136,25 @@ def getMinunumM(a):
                 return i
 
 print(getMinunumM(n))
+
+# %% QUESTAO G DENOVOO
+
+n = int(input())
+
+def isPrime(a):
+    for i in range(2, a):
+        if a % i == 0:
+            return False
+    return True
+
+def getMin(b):
+    if b > 2:
+        for i in range(1,b):
+            if isPrime(b * i + 1) == False:
+                return i
+    elif b == 2:
+        return 4
+    else:
+        return 3
+
+print(getMin(n))
