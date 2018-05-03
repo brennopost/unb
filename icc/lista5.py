ff403a8c1d505efae81eb0ad50072c613a498199
Brenno Cordeiro <brennopost@gmail.com>
2018-05-03 16:40:28 -0300

    lista 5 terminada

--- icc/lista5.py
+++ icc/lista5.py
@@ -38,3 +38,31 @@ for i in range(n):
     u += count(text, 'u')
 
 print("{} {}".format(p, u))
+
+# %% QUESTAO E
+n = input().split('.')
+print(n)
+for i in n:
+    print(i[0:3].title() + i[3:], end=".")
+
+# %% QUESTAO F
+a = input()
+b = input()
+def john(text):
+    o = 0
+    for i in "JOHN":
+        o += text.count(i)
+    return o
+print("{} {}".format(john(a), john(b)))
+
+# %% QUESTAO G
+x = input()
+for i in x:
+    if i.isupper():
+        print('U', end='')
+    elif i.islower():
+        print('L', end='')
+    elif i == ' ':
+        print('W', end='')
+    else:
+        print('D', end='')
