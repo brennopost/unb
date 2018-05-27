33dd3f60668401ab68609623e5af0a1555edb1d8
Brenno Cordeiro <brennopost@gmail.com>
2018-05-27 11:57:04 -0300

    lista7 updates

--- icc/lista7.py
+++ icc/lista7.py
@@ -65,3 +65,74 @@ for i in range(N):
 
 for i in sorted(ex_machina, key=lambda dif: dif[2], reverse=True ):
     print(i[1],end='')
+
+# %% QUESTAO 4
+N = int(input())
+pos = [0,0]
+mov = {'N':0, 'S':0, 'L':0, 'O':0}
+for i in range(N):
+    dir, steps = input().split()
+    mov[dir] += int(steps)
+print("{} {} {} {}".format(max(mov['N'], mov['S']) - mov['N'],
+                           max(mov['N'], mov['S']) - mov['S'],
+                           max(mov['L'], mov['O']) - mov['L'],
+                           max(mov['L'], mov['O']) - mov['O']))
+
+# %% QUESTAO 5
+# usar from collections import Counter para performance
+text = input()
+
+
+def getWords(inp):
+    words = []
+    buffer = []
+    for i in inp:
+        if i not in "., ":
+            buffer.append(i.lower())
+        else:
+            if len(buffer) > 0:
+                words.append("".join(buffer))
+            buffer = []
+    return words
+
+
+wordList = getWords(text)
+output = []
+for i in set(wordList):
+    output.append(((i), wordList.count(i)))
+
+for i in sorted(output, key=lambda tup: tup[1], reverse=True):
+    print("{} {}".format(i[0].capitalize(), i[1]))
+
+# %% QUESTAO 6
+N = int(input())
+papers = []
+for i in range(N):
+    file, otherStuff = input().split(' ', 1)
+    papers.append((file, otherStuff))
+
+keywords = input().split()
+for i in papers:
+    for j in keywords:
+        if j in i[1]:
+            print(i[0])
+
+# TODO HANDLE MULTIPLE KEYWORDS IN SINGLE STRING
+# %% QUESTAO 7
+X, Y = input().split()
+def ack(x, y):
+    if x == 0:
+        return y + 1
+    elif y == 0:
+        return ack(x - 1, 1)
+    else:
+        return ack(x -1, ack(x, y-1))
+
+print(ack(int(X), int(Y)))
+
+# %% QUESTAO 8
+N = int(input())
+plays = []
+for i in range(N):
+    player, index = input().split()
+    plays.append((player, int(index)))
